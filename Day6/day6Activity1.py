db71f827b2d41b82feebd881edf6d65fe04e0489
Emmanuel <santosemmanuel17@gmail.com>
2025-03-04 21:08:27 +0800

    push activity1Day6

--- Day6/day6Activity1.py
+++ Day6/day6Activity1.py
@@ -1,1 +1,29 @@
  #Create a Record Keeping App
+users = {}
+
+while (True):
+    print("choose between this options")
+    print("[A] Add Data")
+    print("[B] Delete Data")
+    print("[C] End")
+
+    option = input("Choose: ")
+
+    if option.lower() == 'a':
+        key = input("Enter Key: ")
+        value = input("Enter Value: ")
+
+        users[key] = value
+
+    elif option.lower() == 'b':
+        key = input("Enter Key: ")
+
+        del users[key]
+
+    else:
+        print("Thank you")
+        break
+
+    for key, value in users.items():
+        print(f"{key}:{value}")
+        
