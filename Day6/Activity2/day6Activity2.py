6bb387cb7e7433b3a18ee920a213fb43a02d6bf7
Emmanuel <santosemmanuel17@gmail.com>
2025-03-07 22:43:41 +0800

    day6activity2

---
+++ Day6/Activity2/day6Activity2.py
@@ -0,0 +1,16 @@
+from GrossSalary import *
+from NetSalary import *
+from SalaryDeductions import *
+
+name = input("Name: ")
+hour = int(input("Hour: "))
+
+print("\nGross Salary: ")
+
+print("\nTax: ")
+loan = float(input("Loan: "))
+health_insurance = float(input("Insurance: "))
+
+print("\nTotal Deductions: ")
+
+print("\nNet Salary: ")
